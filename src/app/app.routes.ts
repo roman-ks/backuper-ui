282b051c0d9d879b26bc9e0a3f3513975d5004fe
roman-ks <80638824+roman-ks@users.noreply.github.com>
2023-12-08 20:34:10 +0200

    move account info to sync-info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MsalGuard } from '@azure/msal-angular';
 import { SyncInfoComponent } from './sync-info/sync-info.component';
-import { inject } from '@angular/core';
 
 
 export const routes: Routes = [
--- src/app/sync-info/sync-info.component.ts
+++ src/app/sync-info/sync-info.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { AccountInfo } from '@azure/msal-browser'
 
 @Component({
   selector: 'app-sync-info',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sync-info.component.html',
   styleUrl: './sync-info.component.scss'
 })
-export class SyncInfoComponent {
+export class SyncInfoComponent implements OnInit {
+  account: AccountInfo | null = null;
 
+ constructor(
+  private authService: MsalService
+ ){}
+
+ ngOnInit(): void {
+   var accounts = this.authService.instance.getAllAccounts()
+   if(accounts.length > 0)
+    this.account = accounts[0]
+ }
 }
